42a3f4d5bf6802bcab6dadb211e408ed04bb5e8e
WUcasey <as4756509@gmail.com>
2023-07-19 20:59:05 +0800

    sheep

---
+++ sheepNsheep/assets/scripts/bgcontrol.ts
@@ -0,0 +1,66 @@
+
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class bgcontrol extends cc.Component {
+
+    @property
+    speed:number=100
+
+    @property
+    width:number = 600 ;
+
+    @property(cc.Node)
+    sheep:cc.Node = null;
+
+    @property
+    jumpduration:number = 0.5;
+
+    @property
+    jumpHeight:number = 70;
+
+    
+
+
+    start () {
+
+        for(let bg of this.node.children){
+            bg.on(cc.Node.EventType.MOUSE_DOWN,()=>{
+                
+                this.sheepjump()
+                
+            })
+        }
+        
+
+    }
+
+    update (dt) {
+        for(let bg of this.node.children){
+
+            bg.x -= this.speed*dt;
+            if(bg.x<-this.width){
+                bg.x += this.width*2;
+            }
+
+        }
+    }
+
+
+    sheepjump(){
+        //檢查羊是否有回到地面
+        if(97>this.sheep.y){
+
+            //跳躍動畫
+            let sheepanima=this.sheep.getComponent(cc.Animation);
+            sheepanima.playAdditive('sheep_down');//不會停止當前所撥放的動畫 所以會jump後回到run
+            //跳躍動作
+            let jumpup = cc.moveBy(this.jumpduration,cc.v2(0,this.jumpHeight)).easing(cc.easeCubicActionOut());
+            let jumpdown = cc.moveBy(this.jumpduration,cc.v2(0,-this.jumpHeight)).easing(cc.easeCubicActionIn());
+            let jumpseq = cc.sequence(jumpup,jumpdown);
+            this.sheep.runAction(jumpseq);
+        }
+        
+    }
+}
